e684fe4601d1524292bb0985630e1b6a23a5c913
JulieGab <jullygabriella.sa@gmail.com>
2024-03-01 18:05:17 -0300

    feat: first commit

---
+++ teste2.js
@@ -0,0 +1,27 @@
+function seqFibonacci(numero) {
+    if (numero === 0 || numero === 1) {
+        return true;
+    }
+    numeroAnterior = 0;
+    numeroAtual = 1;
+
+    while(numeroAtual <= numero) {
+        proximoNumero = numeroAnterior + numeroAtual;
+        if(proximoNumero === numero) {
+            return true;
+        }
+    numeroAnterior = numeroAtual
+    numeroAtual = proximoNumero
+    }
+    return false;
+}
+
+numeroInformado = 9
+
+if (seqFibonacci(Number(numeroInformado))) {
+    console.log('O número faz parte da sequência de Fibonacci.')
+} else {
+    console.log('O número não faz parte da sequência de Fibonacci.')
+}
+
+
